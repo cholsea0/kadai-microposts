968fdde65d9770a1aa1ba4b4beb44d4a96b1f0e6
masanori.yajima <gkmasanori.1@gmail.com>
2020-09-23 12:57:18 +0000

    kadai-microposts

---
+++ app/controllers/favorites_controller.rb
@@ -0,0 +1,17 @@
+class FavoritesController < ApplicationController
+  before_action :require_user_logged_in
+
+  def create
+    micropost = Micropost.find(params[:micropost_id])
+    current_user.favorite(micropost)
+    flash[:success] = 'ツイートをお気に入りに登録する。'
+    redirect_to root_url
+  end
+
+  def destroy
+    micropost = Micropost.find(params[:micropost_id])
+    current_user.unfavorite(micropost)
+    flash[:success] = 'ツイートをお気に入りから解除しました。'
+    redirect_to root_url
+  end
+end
